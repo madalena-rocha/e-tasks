23a15a279c8d9736ba2abde24312c0eed2c85de5
madalena-rocha <rochamada1997m@gmail.com>
2024-01-28 14:22:27 -0300

    defining breakpoints

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -17,10 +17,10 @@ export function SideMenu() {
       </Nav>
 
       <Footer>
-        <img src="https://github.com/rodrigorgtic.png" alt="Foto do usuário" />
+        <img src="https://github.com/madalena-rocha.png" alt="Foto do usuário" />
         <div>
-          <strong>Rodrigo Gonçalves</strong>
-          <small>rodrigo@email.com</small>
+          <strong>Madalena Rocha</strong>
+          <small>madalena@email.com</small>
         </div>
 
         <Button type="button">
