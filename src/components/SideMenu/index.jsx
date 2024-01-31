6e67b236d7151524c92567ee47ac33dd4aadb75d
madalena-rocha <rochamada1997m@gmail.com>
2024-01-31 11:33:40 -0300

    opening and closing the menu

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -2,15 +2,18 @@ import { CheckCircle, House, Folder, PlusSquare, Note, SignOut, X } from "@phosp
 
 import { Container, Footer, Nav, Title, Header, Button } from "./styles";
 
-export function SideMenu() {
+export function SideMenu({ menuIsOpen, onCloseMenu }) {
   return (
-    <Container>
+    <Container data-menu-is-open={menuIsOpen}>
       <Header>
         <Title><CheckCircle /> e-Tasks</Title>
 
-        <Button>
-          <X />
-        </Button>
+        {
+          menuIsOpen &&
+          <Button onClick={onCloseMenu}>
+            <X />
+          </Button>
+        }
       </Header>
 
       <Nav>
